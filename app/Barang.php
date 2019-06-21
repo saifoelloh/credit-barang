1f77aff004365f631f210a5ef666969d51486f88
saifoelloh <saifoelloh@gmail.com>
2019-06-22 01:44:46 +0700

    ubah konfigurasi model agar sesuai dengan tabel barang

--- app/Barang.php
+++ app/Barang.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
+    public $incrementing = false;
+    protected $primaryKey = 'kode';
     protected $fillable = [
+        'kode',
         'nama',
         'harga',
         'jumlah',
