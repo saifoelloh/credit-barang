53421b60d18b124a6c1acf3b99cb918f1f1f7d7a
saifoelloh <saifoelloh@gmail.com>
2019-06-22 02:19:12 +0700

    membuat controller untuk endpoint distributor

--- app/Http/Controllers/DistributorController.php
+++ app/Http/Controllers/DistributorController.php
@@ -7,79 +7,93 @@ use Illuminate\Http\Request;
 
 class DistributorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        try {
+            $result = Distributor::All();
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return view('distributor', [
+            'distributor' => $result
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('distributor.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $findDistributor = Distributor::where('nama', $request->nama)->get();
+        if (sizeof($findDistributor)!=0) {
+            return abort(400);
+        }
+
+        try {
+            $result = Distributor::create([
+                'nama' => $request->nama,
+                'alamat' => $request->alamat,
+            ]);
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return redirect()->route('distributor');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Distributor  $distributor
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Distributor $distributor)
+    public function edit($id)
     {
-        //
+        try {
+            $result = Distributor::find($id);
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        if ($result==null) {
+            return abort(404);
+        }
+
+        return view('distributor.edit', [
+            'distributor' => $result
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Distributor  $distributor
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Distributor $distributor)
+    public function update(Request $request, $id)
     {
-        //
+        $findDistributor = Distributor::where('nama', $request->nama)->get();
+        if (sizeof($findDistributor)!=1) {
+            return abort(400);
+        }
+
+        try {
+            $findDistributor->update([
+                'nama' => $request->nama,
+                'alamat' => $request->alamat,
+            ]);
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return redirect()->route('distributor');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Distributor  $distributor
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Distributor $distributor)
+    public function destroy($id)
     {
-        //
-    }
+        $findDistributor = Distributor::where('nama', $request->nama)->get();
+        if (sizeof($findDistributor)!=1) {
+            return abort(400);
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Distributor  $distributor
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Distributor $distributor)
-    {
-        //
+        try {
+            $findDistributor->delete();
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return redirect()->route('distributor');
     }
 }
