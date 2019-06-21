4ce119558378d80241534b2b1844e87f358f7b43
baihaqilp <baihaqiluhurpangestika@gmail.com>
2019-06-22 02:04:19 +0700

    Add model,Controller, migrate, Pembelian

--- app/BarangPembelian.php
+++ app/BarangPembelian.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class BarangPembelian extends Model
 {
-    //
+    protected $fillable = [
+        'pelanggan_id',
+        'kd_barang',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function barang()
+    {
+        return $this->belongsToMany('App\Barang');
+    }
+
+    public function custommer()
+    {
+        return $this->belongsToMany('App\Custommer');
+    }
 }
