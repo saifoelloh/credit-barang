f213b49c60b23cbf02ba93cf665ae68c0dc1258c
Dimas Saputro <112201605596@mhs.dinus.ac.id>
2019-06-22 01:39:58 +0700

    tambah data table

--- resources/views/layouts/barang/index.blade.php
+++ resources/views/layouts/barang/index.blade.php
@@ -3,9 +3,35 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Barang</h1>
 @stop
 
 @section('content')
-    <p>Mashok Bang</p>
+<div class="row">
+<div class="col-md-8">
+<div class="box box-info">
+<div class="box-body">
+<table class="data-table">
+    <thead>
+        <tr>
+            <th>Nama Barang</th>
+            <th>Harga</th>
+            <th>Jumlah</th>
+            <th>Pegawai</th>
+            <th>Distributor</th>
+        </tr>
+    <thead>
+</table>
+</div>
+</div>
+</div>
+</div>
+@stop
+
+@section('js')
+    <script>
+        $(document).ready(function () {
+            $('.data-table').dataTable();
+        });
+    </script>
 @stop
