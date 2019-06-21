f5f34a3da2a16908668e0544698b63c707efdeff
saifoelloh <saifoelloh@gmail.com>
2019-06-22 01:36:51 +0700

    membuat controller untuk endpoint barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,83 +3,101 @@
 namespace App\Http\Controllers;
 
 use App\Barang;
+use App\Distributor;
 use Illuminate\Http\Request;
 
 class BarangController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        $barangs = Barang::All();
+
+        return view('welcome', [
+            'barangs' => $barangs
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $findItem = Barang::where('kode', $request->kode)-get();
+        if (sizeof($findItem)) {
+            return abort(400);
+        }
+
+        $findDistributor = Distributor::find($request->distributorId);
+
+        try {
+            $findDistributor->barang()->create([
+                'kode' => $request->kode,
+                'nama' => $request->nama,
+                'harga' => $request->harga,
+                'jumlah' => $request->kode,
+            ]);
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return redirect()->route('barang');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Barang  $barang
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Barang $barang)
+    public function edit($id)
     {
-        //
+        try {
+            $result = Barang::find($id);
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return view('barang.edit', [
+            'barang' => $result
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Barang  $barang
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Barang $barang)
+    public function update(Request $request, $id)
     {
-        //
+        $findItem = Barang::where('kode', $id)-get();
+        if (sizeof($findItem)) {
+            return abort(404);
+        }
+
+        $findDistributor = Distributor::find($request->distributorId);
+        if ($findItem[0]->id!=$findDistributor->id) {
+            return abort(403);
+        }
+
+        try {
+            $findDistributor->barang()->update([
+                'kode' => $request->kode,
+                'nama' => $request->nama,
+                'harga' => $request->harga,
+                'jumlah' => $request->kode,
+            ]);
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return redirect()->route('barang');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Barang  $barang
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Barang $barang)
+    public function destroy($id)
     {
-        //
-    }
+        $findItem = Barang::where('kode', $id)-get();
+        if (sizeof($findItem)) {
+            return abort(404);
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Barang  $barang
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Barang $barang)
-    {
-        //
+        try {
+            $findItem->delete();
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return redirect()->action('barang');
     }
 }
