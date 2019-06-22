bd6c40481cc9f99766820f1b4d740673e30f9e63
saifoelloh <saifoelloh@gmail.com>
2019-06-22 22:01:57 +0700

    ubah data statis dengan integrasi sistem

--- resources/views/layouts/pelanggan/edit.blade.php
+++ resources/views/layouts/pelanggan/edit.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="col-md-6 text-right">
         <h3>
-            <button type="button" class="btn btn-primary" 
+            <button type="button" class="btn btn-primary"
                 onclick="window.location.href='/admin/pelanggan/'">
                 Kembali
             </button>
@@ -20,23 +20,25 @@
 
 @section('content')
     <div class="box box-primary">
-        <form role="form">
+        <form role="form" action={{route('custommer.update', $custommer->id)}} method="post">
+            @method("PUT")
+            @csrf
             <div class="box-body">
-            <div class="form-group">
-                <label for="nameCust">Nama Pelanggan</label>
-                <input type="text" class="form-control" id="nameCust" placeholder="Nama Terang Pelanggan">
-            </div>
-            <div class="form-group">
-                <label for="addrCust">Alamat</label>
-                <input type="text" class="form-control" id="addrCust" placeholder="Alamat Pelanggan Saat Ini">
-            </div>
-            <div class="form-group">
-                <label for="phoneCust">Nomor Telp</label>
-                <input type="text" class="form-control" id="phoneCust" placeholder="Nomor Telepon Yang Dapat Dihubungi">
-            </div>
+                <div class="form-group">
+                    <label for="nama">Nama Pelanggan</label>
+                    <input type="text" name="nama" value="{{ $custommer->nama }}" class="form-control" id="nama" placeholder="Nama Terang Pelanggan">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <input type="text" name="alamat" value="{{ $custommer->alamat }}" class="form-control" id="alamat" placeholder="Alamat Pelanggan Saat Ini">
+                </div>
+                <div class="form-group">
+                    <label for="no_telp">Nomor Telp</label>
+                    <input type="text" name="no_telp" value="{{ $custommer->no_telp }}" class="form-control" id="no_telp" placeholder="Nomor Telepon Yang Dapat Dihubungi">
+                </div>
             </div>
             <div class="box-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </form>
     </div>
