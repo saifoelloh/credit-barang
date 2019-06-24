e22ab05a4f175f790dcf4da5ad6c251c66647549
saifoelloh <saifoelloh@gmail.com>
2019-06-25 02:36:19 +0700

    menghubungkan tampilan barang dengan controller

--- resources/views/layouts/barang/index.blade.php
+++ resources/views/layouts/barang/index.blade.php
@@ -9,10 +9,9 @@
     </div>
     <div class="col-md-6 text-right">
         <h3>
-            <button type="button" class="btn btn-info"
-                onclick="window.location.href='/admin/barang/add'">
+            <a class="btn btn-info" href={{route('barang.create')}}>
                 Tambah Barang
-            </button>
+            </a>
         </h3>
     </div>
 </div>
@@ -26,13 +25,12 @@
                 <table class="data-table table table-bordered table-hover">
                     <thead>
                         <tr>
-                            <th>No</th>
-                            <th>Nama Barang</th>
-                            <th>Harga</th>
-                            <th>Jumlah</th>
-                            <th>Pegawai</th>
-                            <th>Distributor</th>
-                            <th>Tindakan</th>
+                            <th class="text-center">No</th>
+                            <th class="text-center">Nama Barang</th>
+                            <th class="text-center">Harga</th>
+                            <th class="text-center">Jumlah</th>
+                            <th class="text-center">Distributor</th>
+                            <th class="text-center">Tindakan</th>
                         </tr>
                     <thead>
                     <tbody>
@@ -41,7 +39,6 @@
                             <td>Ticket VIP Gfriend</td>
                             <td>1.500.000</td>
                             <td>5</td>
-                            <td>saipul jamil</td>
                             <td>PT Astrindo</td>
                             <td><button type="button" class="btn btn-primary btn-xs" onclick="window.location.href='/admin/barang/edit'">Edit</button>
                                 <button type="button" class="btn btn-danger btn-xs">Delete</button>
@@ -58,7 +55,24 @@
 @section('js')
     <script>
         $(document).ready(function () {
-            $('.data-table').dataTable();
+          $('.data-table').dataTable({
+            data: {!! $barangs !!},
+            columns: [
+              {data: 'id'},
+              {data: 'nama'},
+              {data: 'harga'},
+              {data: 'jumlah'},
+              {
+                data: 'id',
+                render: function(data) {
+                  const link = 'http://localhost:8000/admin/barang/'+data;
+                  const detail = '<a class="btn btn-primary btn-xs" style="margin: 0 3px" href="'+link+'">edit</a>';
+                  const hapus = '<form role="form" action="'+link+'" style="margin: 0 3px;display: inline" method="POST">@method("DELETE")@csrf<button class="btn btn-danger btn-xs">delete</button></form>';
+                  return '<div class="text-center">'+detail+hapus+'</div>';
+                }
+              },
+            ]
+          });
         });
     </script>
 @stop
