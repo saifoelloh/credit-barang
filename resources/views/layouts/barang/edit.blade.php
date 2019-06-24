3970cd7fd6bb0618101f5e4c0a5a9a0b7b867bbe
catzy007 <catzy.rais@gmail.com>
2019-06-24 18:47:23 +0700

    update editbarang

--- resources/views/layouts/barang/edit.blade.php
+++ resources/views/layouts/barang/edit.blade.php
@@ -19,25 +19,41 @@
 @stop
 
 @section('content')
-    <div class="box box-primary">
-        <form role="form">
-            <div class="box-body">
-                <div class="form-group">
-                    <label for="nameBrg">Nama Barang</label>
-                    <input type="text" class="form-control" id="nameBrg" placeholder="Nama Barang" value="Ticket GFriend">
-                    <label for="priceBrg">Harga</label>
-                    <input type="number" min="0.01" step="0.01" class="form-control" id="priceBrg" placeholder="Harga Barang" value="1500000">
-                    <label for="ammtBrg">Jumlah</label>
-                    <input type="number" class="form-control" id="ammtBrg" placeholder="Jumlah Barang" value="5">
-                    <label for="pgwiBrg">Pegawai</label>
-                    <input type="text" class="form-control" id="pgwiBrg" placeholder="Pegawai Input Barang" value="saipul jamil">
-                    <label for="distbBrg">Distributor</label>
-                    <input type="text" class="form-control" id="distbBrg" placeholder="Distributor Barang" value="PT Astrindo">
-                </div>
+<div class="box box-primary">
+    <form role="form">
+        <div class="box-body">
+            <div class="form-group">
+                <label for="nameBrg">Nama Barang</label>
+                <input type="text" class="form-control" id="nameBrg" placeholder="Nama Barang" value="Ticket GFriend">
+                <label for="priceBrg">Harga</label>
+                <input type="number" min="0.01" step="0.01" class="form-control" id="priceBrg" placeholder="Harga Barang" value="1500000">
+                <label for="ammtBrg">Jumlah</label>
+                <input type="number" class="form-control" id="ammtBrg" placeholder="Jumlah Barang" value="5">
+                <label for="pgwiBrg">Pegawai</label>
+                <select class="form-control select2" id="pgwiBrg" style="width: 100%;">
+                    <option selected="selected" disabled="disabled">Pegawai Input Barang</option>
+                    <option>Amrin</option>
+                    <option>Saipul Jamil</option>
+                    <option>Udin</option>
+                    <option>Ipan</option>
+                    <option>Rijal</option>
+                    <option>Apit</option>
+                </select>
+                <label for="distbBrg">Distributor</label>
+                <select class="form-control select2" id="distbBrg" style="width: 100%;">
+                  <option selected="selected" disabled="disabled">Distributor Barang</option>
+                  <option>PT Astrindo</option>
+                  <option>Tirta Air Modeal</option>
+                  <option>Bintang Sinar Sentosa</option>
+                  <option>Raja Besi</option>
+                  <option>Valve Corporation</option>
+                  <option>Bunga Cipta Komputer</option>
+                </select>
             </div>
-            <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
-    </div>
+        </div>
+        <div class="box-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</div>
 @stop
