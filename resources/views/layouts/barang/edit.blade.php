fc16364a173c8b287d07e852a2f0c38c3ace34f2
saifoelloh <saifoelloh@gmail.com>
2019-06-26 14:44:44 +0700

    menyelesaikan tampilan edit barang dan integrasi dengan controller

--- resources/views/layouts/barang/edit.blade.php
+++ resources/views/layouts/barang/edit.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="col-md-6 text-right">
         <h3>
-            <button type="button" class="btn btn-primary" 
+            <button type="button" class="btn btn-primary"
                 onclick="window.location.href='/admin/barang/'">
                 Kembali
             </button>
@@ -20,35 +20,19 @@
 
 @section('content')
 <div class="box box-primary">
-    <form role="form">
+    <form role="form" action="{{route('barang.update', $barang->kode)}}" method="POST">
+        @method('PUT')
+        @csrf
         <div class="box-body">
             <div class="form-group">
+                <label for="kodeBrg">Kode Barang</label>
+                <input name="kode" type="text" class="form-control" id="kodeBrg" placeholder="Nama Barang" value="{{$barang->kode}}" required>
                 <label for="nameBrg">Nama Barang</label>
-                <input type="text" class="form-control" id="nameBrg" placeholder="Nama Barang" value="Ticket GFriend">
+                <input type="text" class="form-control" id="nameBrg" placeholder="Nama Barang" name="nama" value="{{$barang->nama}}" required>
                 <label for="priceBrg">Harga</label>
-                <input type="number" min="0.01" step="0.01" class="form-control" id="priceBrg" placeholder="Harga Barang" value="1500000">
+                <input type="number" min="0.01" step="0.01" class="form-control" id="priceBrg" placeholder="Harga Barang" name="harga" value="{{$barang->harga}}" required>
                 <label for="ammtBrg">Jumlah</label>
-                <input type="number" class="form-control" id="ammtBrg" placeholder="Jumlah Barang" value="5">
-                <label for="pgwiBrg">Pegawai</label>
-                <select class="form-control select2" id="pgwiBrg" style="width: 100%;">
-                    <option selected="selected" disabled="disabled">Pegawai Input Barang</option>
-                    <option>Amrin</option>
-                    <option>Saipul Jamil</option>
-                    <option>Udin</option>
-                    <option>Ipan</option>
-                    <option>Rijal</option>
-                    <option>Apit</option>
-                </select>
-                <label for="distbBrg">Distributor</label>
-                <select class="form-control select2" id="distbBrg" style="width: 100%;">
-                  <option selected="selected" disabled="disabled">Distributor Barang</option>
-                  <option>PT Astrindo</option>
-                  <option>Tirta Air Modeal</option>
-                  <option>Bintang Sinar Sentosa</option>
-                  <option>Raja Besi</option>
-                  <option>Valve Corporation</option>
-                  <option>Bunga Cipta Komputer</option>
-                </select>
+                <input type="number" class="form-control" id="ammtBrg" placeholder="Jumlah Barang" name="jumlah" value="{{$barang->jumlah}}" required>
             </div>
         </div>
         <div class="box-footer">
